d1114daf1491247ae1eedbe03a7a9808f5327c25
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2023-12-12 14:52:38 -0300

    Added test for user creation

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,14 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from app import schemas
+from .database import session, client
 
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json() == {"message": "Hello!"}
+    assert res.status_code == 200
+
+def test_create_user(client):
+    res = client.post("/alchemy/users/", json={"email": "hello@gmail.com", "password": "pass123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert res.json().get("email") == "hello@gmail.com"
+    assert res.status_code == 201
